fbcc8dfd5f0418132dad03ac4fbaf6a692c74eaf
Vikas Pandey <vikaspa6@gmail.com>
2019-07-27 18:53:41 +0530

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,9 @@
+
+# - Constants --------------------------------------------------------
+$LogDir            = "C:\LogFiles"
+$RegistryKey      = "HKLM:\SOFTWARE\COMP_NAME\Applications"
+$TemplateDate      = "08-11-2016"
+
+# - Script variables -------------------------------------
+$ScriptDir       = Split-Path $MyInvocation.MyCommand.Path
+$ScriptName      = $MyInvocation.MyCommand
