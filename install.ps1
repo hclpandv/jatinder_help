982b83b7f1612c3449103fefb4f3d85d566447c1
Vikas Pandey <vikaspa6@gmail.com>
2019-07-27 18:57:51 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,23 @@
+#--------------
+#---- Functions
+#--------------
+Function Write-Log {
+    Param (
+        [Parameter(Mandatory = $True, Position = 0)] [String] $Message
+    )    
+    # Create log directory when needed
+    If ((Test-Path -Path $LogDir) -eq $False) {
+        New-Item $LogDir -Type Directory | Out-Null
+    }
+    $TimeStamp = "[" + (Get-Date -Format dd-MM-yy) + "," + (Get-Date -Format HH:mm:ss) + "] "
+    $Log = $TimeStamp + $Message 
+    # Write to screen
+    Write-Host -Object $Message   
+    # Write to log file
+    Add-Content -Path $LogFile -Value $Log
+}
+
+
 
 # - Constants --------------------------------------------------------
 $LogDir            = "C:\LogFiles"
